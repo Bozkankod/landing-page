f7fe561610322ed28a64208717a006fd7d9533a8
bozkankod <bozkankod@gmail.com>
2023-07-01 23:11:48 +0300

    projeler eklendi.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import burcApi from "../../Assets/Projects/burcapi.png";
+import lolKlipleri from "../../Assets/Projects/lolKlipleri.png";
 import scrumPoint from "../../Assets/Projects/scrumpoint.png";
 
 function Projects() {
@@ -23,7 +24,6 @@ function Projects() {
               isBlog={false}
               title="Scrum Point"
               description="Scrum puanlama yapılabilmesi için tasarladığım ilk net ve somut projem. React.js, Firebase ve Firestore kullanıyorum."
-              ghLink="https://github.com/bozkankod/scrumpoker"
               demoLink="https://scrumpoint.app/"
             />
           </Col>
@@ -35,6 +35,27 @@ function Projects() {
               title="Burç API"
               description="Günlük, haftalık ve aylık olarak tüm burçların güncel yorumları, özellikleri gibi tüm datayı dinamik olarak url yapısıyla fetch eder ve json formatında döndürür."
               ghLink="https://github.com/bozkankod/burcapi"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={burcApi}
+              isBlog={false}
+              title="Randomize Files Names"
+              description="İlgili dizindeki tüm dosyaların başına 0-10000 numara aralığında rastgele rakamlar ekler."
+              ghLink="https://github.com/bozkankod/randomizefiles"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={lolKlipleri}
+              isBlog={false}
+              title="Lol Klipleri"
+              description="Bu youtube kanalı tamamen AI tarafından yönetiliyor, tüm video bulma, edit işlemleri dahil. Abone olmak isterseniz link aşağıda."
+              ghLink="https://github.com/bozkankod/randomizefiles"
+              demoLink="https://www.youtube.com/@lolklipleri"
             />
           </Col>
 
